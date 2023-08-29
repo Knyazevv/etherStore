f1287a0145de42d528af2f8a4259e83d8a7d2d4e
Knyazevv <dreq12345@gmail.com>
2023-08-29 19:42:32 +0300

    use account

--- components/providers/web3/hooks/useAccount.js
+++ components/providers/web3/hooks/useAccount.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import useSWR from "swr";
 
 const adminAddresses = {
-  "0x0417269e7b61af70fa3a5bd3515e5d6aafb92fb12abf0f2efca6fe1da791aa79": true,
+  "0xd66c4591fcbcdd1c1bbe0aa8e02a81f708ba81d744c0fbbddefbbba42c49a05c": true,
 };
 
 export const handler = (web3, provider) => () => {
